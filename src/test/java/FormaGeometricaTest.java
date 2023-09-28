e9e357aef6c32f04f125c102fb594991a20c4083
Lais Linhares <lais.linhares@ufjf.br>
2023-09-27 23:01:19 -0300

    update

--- src/test/java/FormaGeometricaTest.java
+++ src/test/java/FormaGeometricaTest.java
@@ -1,18 +1,19 @@
 import org.example.FormaGeometrica;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FormaGeometricaTest {
 
     @Test
-    void deveRetornarNomePoligono() {
+    public void deveRetornarNomePoligono() {
         FormaGeometrica formaGeometrica = new FormaGeometrica();
         formaGeometrica.setPoligono("Triangulo");
         assertEquals("Triangulo", formaGeometrica.getPoligono());
     }
 
     @Test
-    void deveRetornarLadosPoligono() {
+    public void deveRetornarLadosPoligono() {
         FormaGeometrica formaGeometrica = new FormaGeometrica();
         formaGeometrica.setPoligono("Triangulo");
         assertEquals(3, formaGeometrica.getLados());
